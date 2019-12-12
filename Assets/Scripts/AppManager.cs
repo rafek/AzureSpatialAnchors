c89d2120329c68abd8d531f77e29e1fbe1afd8e6
rafek <rafal.legiedz@gmail.com>
2019-12-12 12:27:14 +0100

    Android sample app for Azure Spatial Anchors.

---
+++ Assets/Scripts/AppManager.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Azure.SpatialAnchors.Unity;
+using System;
+using TMPro;
+using UnityEngine;
+
+public class AppManager : MonoBehaviour
+{
+    [SerializeField]
+    private SpatialAnchorManager spatialAnchorManager;
+
+    [SerializeField]
+    private TextMeshProUGUI feedbackText;
+
+    private void Awake()
+    {
+        Application.logMessageReceivedThreaded += Application_logMessageReceivedThreaded;
+    }
+
+    private void OnDestroy()
+    {
+        Application.logMessageReceivedThreaded -= Application_logMessageReceivedThreaded;
+    }
+
+    private void Application_logMessageReceivedThreaded(string condition, string stackTrace, LogType type)
+    {
+        UnityDispatcher.InvokeOnAppThread(() => feedbackText.text += $"\nLOG: {condition}.");
+    }
+
+    public async void CreateSession()
+    {
+        feedbackText.text += $"\nCreating session.";
+
+        try
+        {
+            await spatialAnchorManager.CreateSessionAsync();
+
+            feedbackText.text += $"\nSession created.";
+        }
+        catch (Exception ex)
+        {
+            feedbackText.text += $"\nError while creating session: {ex.Message}";
+        }
+    }
+}
